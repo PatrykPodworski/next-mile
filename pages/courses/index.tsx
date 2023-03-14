e84e969cae18cb84d1f8100ea3fba3bf1338c6fd
Patryk Podworski <patryk@xtramile.no>
2023-03-14 23:37:30 +0100

    Get fake data

--- pages/courses/index.tsx
+++ pages/courses/index.tsx
@@ -1,18 +1,44 @@
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Head from "next/head";
+import { Fragment } from "react";
 
-const Courses = ({}: CoursesProps) => (
+const Courses = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => (
   <>
     <Head>
       <title>Courses list</title>
     </Head>
-    <div className="flex flex-col items-center">
-      <p className="text-xl font-bold text-amber-500">
-        This is the courses list page.
-      </p>
+    <div className="flex flex-col">
+      {data.map((x) => (
+        <Fragment key={x.id}>
+          <p className="text-xl font-bold text-amber-500">{x.title}</p>
+          <p className="text-base text-black">{x.description}</p>
+        </Fragment>
+      ))}
     </div>
   </>
 );
 
-type CoursesProps = {};
+export const getStaticProps = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data: CoursesApiResponse[] = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
 
 export default Courses;
+
+type CoursesApiResponse = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+};
