3b7824d45e43b1a788673a5f93dcecf35d625c2f
Patryk Podworski <patryk@xtramile.no>
2023-03-22 23:33:05 +0100

    course details page

--- pages/courses/[id].tsx
+++ pages/courses/[id].tsx
@@ -1,9 +1,12 @@
+import ChevronLeft from "@/components/icons/ChevronLeft";
 import {
   GetStaticPaths,
   GetStaticPropsContext,
   InferGetStaticPropsType,
 } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import { CoursesApiResponse } from ".";
 
 const Course = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
@@ -16,7 +19,30 @@ const Course = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
       <Head>
         <title>{data.title}</title>
       </Head>
-      <h1 className="text-4xl font-bold my-8 text-neutral-900">{data.title}</h1>
+      <Link
+        href={"/courses"}
+        className="flex gap-2 text-base text-blue-300 hover:text-blue-500 transition-all duration-300 items-center"
+      >
+        <ChevronLeft className="w-4 h-4" /> Go back
+      </Link>
+      <div className="flex gap-4 my-8">
+        <div className="relative aspect-video w-80 shadow-xl overflow-hidden">
+          <Image
+            src={data.image}
+            alt={data.title}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold mb-4 text-neutral-900">
+            {data.title}
+          </h1>
+          <p className="text-lg text-na900 my-4">{data.description}</p>
+          <p className="text-lg text-neutral-700 italic">{`${data.price} zł`}</p>
+        </div>
+      </div>
+      <p className="text-lg text-neutral-700">{data.longDescription}</p>
     </>
   );
 };
@@ -57,7 +83,13 @@ export const getStaticProps = async ({
 };
 
 type CourseDetailsResponse = {
+  id: string;
   title: string;
+  price: number;
+  description: string;
+  longDescription: string;
+  category: string;
+  image: string;
 };
 
 export default Course;
