1b887f805707648f27f53b3610ed8f2d55599fbf
Patryk Podworski <patryk@xtramile.no>
2023-05-10 23:27:18 +0200

    graphql init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,22 @@ import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import SEO from "../next-seo.config";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/graphql/apolloClient";
 
 const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <QueryClientProvider client={queryClient}>
-    <DefaultSeo {...SEO} />
-    <CartContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </CartContextProvider>
-  </QueryClientProvider>
+  <ApolloProvider client={client}>
+    <QueryClientProvider client={queryClient}>
+      <DefaultSeo {...SEO} />
+      <CartContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </CartContextProvider>
+    </QueryClientProvider>
+  </ApolloProvider>
 );
 
 export default App;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      name
+      price
+      description
+      images {
+        url
+      }
+    }
+  }
+`;
+
 const About = () => {
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
   return (
     <>
       <div className="flex flex-col items-center">
         <p className="text-xl font-bold text-violet-500">
           This is the about page.
         </p>
+
+        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </>
   );
