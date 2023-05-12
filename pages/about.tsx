1c8eee31a0afd743f10a535862fb3d3f77abe3a5
Patryk Podworski <patryk@xtramile.no>
2023-05-13 01:34:39 +0200

    Remove debug code

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,24 +2,12 @@ import { GetProductsDocument } from "@/graphql/generated/graphql";
 import { useQuery } from "@apollo/client";
 
 const About = () => {
-  const { loading, error, data } = useQuery(GetProductsDocument);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
   return (
     <>
       <div className="flex flex-col items-center">
         <p className="text-xl font-bold text-violet-500">
           This is the about page.
         </p>
-
-        <pre>{JSON.stringify(data, null, 2)}</pre>
       </div>
     </>
   );
