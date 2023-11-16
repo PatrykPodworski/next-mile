95c9569e3de6dbbf8cb0724d84e9a3a4e1db7975
Patryk Podworski <patryk@xtramile.no>
2023-11-16 19:53:14 +0100

    Simple protected my orders page

---
+++ pages/orders/my.tsx
@@ -0,0 +1,13 @@
+const MyOthersPage = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <p className="text-xl font-bold text-purple-500">
+          This is my orders page.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default MyOthersPage;
