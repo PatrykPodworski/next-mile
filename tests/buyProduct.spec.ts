3aa276ba083f0556eee7b48e1b7bb1cd39c23112
Patryk Podworski <patryk@xtramile.no>
2023-11-29 23:24:31 +0100

    Random change to deploy new version

--- tests/buyProduct.spec.ts
+++ tests/buyProduct.spec.ts
@@ -4,7 +4,7 @@ test("can buy product", async ({ page }) => {
   test.setTimeout(60000);
   const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL;
   if (!baseUrl) {
-    throw new Error("PLAYWRIGHT_TEST_BASE_URL is not set");
+    throw new Error("PLAYWRIGHT_TEST_BASE_URL is missing");
   }
 
   await page.goto(baseUrl);
