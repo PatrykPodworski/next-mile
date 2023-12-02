3a0e44a19742d9dd7c83889f62ba9e9dbf8b2fcd
Patryk Podworski <patryk@xtramile.no>
2023-12-02 01:12:46 +0100

    replace index text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,10 @@ const Home = () => {
     <>
       <div className="flex flex-col items-center">
         <p className="text-xl font-bold text-green-500">
-          This is the index page.
+          Welcome to Our Online Store!
+        </p>
+        <p className="text-lg text-gray-600">
+          Explore our wide range of courses and find the perfect topics for you.
         </p>
       </div>
     </>
