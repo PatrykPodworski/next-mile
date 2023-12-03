b1d6582e4ec8f0dc74555a9cfdd4ce8669b4d353
Patryk Podworski <patryk@xtramile.no>
2023-12-03 01:53:05 +0100

    add timeout for wait for url

--- tests/buyProduct.spec.ts
+++ tests/buyProduct.spec.ts
@@ -27,10 +27,13 @@ test("can buy product", async ({ page }) => {
   await page.fill('[id="billingName"]', "Patryk Playwright");
   await page.click('[data-testid="hosted-payment-submit-button"]');
 
-  await page.waitForURL((url) => {
-    console.log(url.href, "vs", `${baseUrl}/orders/success`);
-    return url.href.startsWith(`${baseUrl}/orders/success`);
-  });
+  await page.waitForURL(
+    (url) => {
+      console.log(url.href, "vs", `${baseUrl}/orders/success`);
+      return url.href.startsWith(`${baseUrl}/orders/success`);
+    },
+    { timeout: 120000 }
+  );
 
   const heading = page.locator("h1");
   await expect(heading).toHaveText("Success!");
