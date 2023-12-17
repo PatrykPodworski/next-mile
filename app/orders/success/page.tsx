f71650d258f791fefdbb2031a07e998501f06b05
Patryk Podworski <patryk@xtramile.no>
2023-12-17 22:25:24 +0100

    migrate success page

---
+++ app/orders/success/page.tsx
@@ -0,0 +1,12 @@
+const SuccessPage = () => {
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <h1 className="text-4xl font-bold" data-testid="success-heading">
+        Success!
+      </h1>
+      <p className="text-lg">Your order has been processed.</p>
+    </main>
+  );
+};
+
+export default SuccessPage;
