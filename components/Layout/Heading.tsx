da4802e88c2fe85c93d2e586076e8a22df5f8808
Patryk Podworski <patryk@xtramile.no>
2024-01-08 00:26:11 +0100

    Pass data test id

--- components/Layout/Heading.tsx
+++ components/Layout/Heading.tsx
@@ -1,8 +1,10 @@
 import clsx from "clsx";
 import { ReactNode } from "react";
 
-const Heading = ({ children, className }: ComponentProps) => (
-  <h1 className={clsx("text-4xl font-bold", className)}>{children}</h1>
+const Heading = ({ children, className, ...props }: ComponentProps) => (
+  <h1 className={clsx("text-4xl font-bold", className)} {...props}>
+    {children}
+  </h1>
 );
 
 export default Heading;
